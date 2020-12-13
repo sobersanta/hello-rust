90722bb3ccd6553cb2896fe9b96c66bc822c4e73
derar <derar@appsflyer.com>
2020-12-14 00:39:48 +0200

    omg

--- src/exercises/list_nums.rs
+++ src/exercises/list_nums.rs
@@ -15,47 +15,42 @@ impl ListNode {
 }
 
 pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut l1 = &l1;
-    let mut l2 = &l2;
+    let mut l1 = l1;
+    let mut l2 = l2;
 
     let mut head = None;
     let mut rem = 0;
-    let mut tail = &mut head;
+    let mut tail = head.as_mut();
 
     while l1.is_some() || l2.is_some() {
-        let d1 = match l1 {
-            None => 0,
-            Some(ref d) => d.val,
-        };
-        let d2 = match l2 {
-            None => 0,
-            Some(ref d) => d.val,
-        };
-        let sum = d1 + d2 + rem;
-        let val = sum % 10;
+        let mut sum = rem;
+        if let Some(d) = l1 {
+            l1 = d.next;
+            sum += d.val;
+        }
+        if let Some(d) = l2 {
+            l2 = d.next;
+            sum += d.val;
+        }
+
         rem = sum / 10;
 
-        let node = Some(Box::new(ListNode::new(val)));
+        let node = Some(Box::new(ListNode::new(sum % 10)));
         match tail {
             None => {
                 head = node;
-                tail = &mut head;
+                tail = head.as_mut();
             }
             Some(n) => {
                 n.next = node;
-                tail = &mut n.next;
+                tail = n.next.as_mut();
             }
-        }
-
-        if let Some(d) = l1 {
-            l1 = &d.next;
-        }
-        if let Some(d) = l2 {
-            l2 = &d.next;
         }
     }
     if rem != 0 {
-        tail.as_mut().map(|node| node.next = Some(Box::new(ListNode::new(rem))));
+        if let Some(node) = tail {
+            node.next = Some(Box::new(ListNode::new(rem)));
+        }
     }
 
     head
